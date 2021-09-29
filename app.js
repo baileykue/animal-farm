b8f87b0094334f187a0fdc5bd607fdd4c16c722d
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-09-29 16:31:53 -0700

    added images, sound when images are clicked, and sounds with key assignments

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 // import functions and grab DOM elements
+const horseImage = document.getElementById('horse-image');
+const horseSound = document.getElementById('horse-sound');
+const dogImage = document.getElementById('dog-image');
+const dogSound = document.getElementById('dog-sound');
+const catImage = document.getElementById('cat-image');
+const catSound = document.getElementById('cat-sound');
 
 // initialize global state
 
@@ -6,3 +12,43 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+horseImage.addEventListener('click', ()=>{
+  console.log('clicking the horse!');
+  horseSound.play();
+}
+);
+
+dogImage.addEventListener('click', ()=>{
+  console.log('clicking the dog!');
+  dogSound.play();
+  dogImage.classList.add('')
+}
+);
+
+catImage.addEventListener('click', ()=>{
+  console.log('clicking the cat!');
+  catSound.play();
+}
+);
+
+window.addEventListener('keydown', (event)=>{
+  if (event.key =="h") {
+    horseSound.play();
+  }
+}
+);
+
+window.addEventListener('keydown', (event)=>{
+  if (event.key =="d") {
+    dogSound.play();
+  }
+}
+);
+
+window.addEventListener('keydown', (event)=>{
+  if (event.key =="c") {
+    catSound.play();
+  }
+}
+);
+
