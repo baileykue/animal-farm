841ed327d8dace395fc85d42bac3518b4311db69
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-06-06 12:48:05 -0700

    cleaning up code and updating read me

--- app.js
+++ app.js
@@ -1,44 +1,38 @@
-
 const goatImage = document.getElementById('goat-image');
 const goatSound = document.getElementById('goat-sound');
+
 const chickenImage = document.getElementById('chicken-image');
 const chickenSound = document.getElementById('chicken-sound');
+
 const cowImage = document.getElementById('cow-image');
 const cowSound = document.getElementById('cow-sound');
 
-goatImage.addEventListener('click', ()=>{
+goatImage.addEventListener('click', () => {
   goatSound.play();
-}
-);
+});
 
-chickenImage.addEventListener('click', ()=>{
+chickenImage.addEventListener('click', () => {
   chickenSound.play();
-}
-);
+});
 
-cowImage.addEventListener('click', ()=>{
+cowImage.addEventListener('click', () => {
   cowSound.play();
-}
-);
+});
 
-window.addEventListener('keydown', (event)=>{
-  if (event.key =="g") {
+window.addEventListener('keydown', (event) => {
+  if (event.key === 'g') {
     goatSound.play();
   }
-}
-);
+});
 
-window.addEventListener('keydown', (event)=>{
-  if (event.key =="c") {
+window.addEventListener('keydown', (event) => {
+  if (event.key === 'c') {
     chickenSound.play();
   }
-}
-);
+});
 
-window.addEventListener('keydown', (event)=>{
-  if (event.key =="m") {
+window.addEventListener('keydown', (event) => {
+  if (event.key === 'm') {
     cowSound.play();
   }
-}
-);
-
+});
